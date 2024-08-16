af028e098b07d785042c4c883eb474fbcaa910cd
Chad W Pry <chad.pry@gamil.com>
2024-08-16 15:46:53 -0500

    Add installation instructions into README (wip)

--- README.md
+++ README.md
@@ -2,4 +2,49 @@
 
 ## Setup
 
-Configure $GITUSER, $GITEMAIL for git configuration
+### Setup $XDG_CONFIG_HOME
+
+Add base directory
+
+```zsh
+mkdir -p ~/.config
+```
+
+### Nix
+
+Install nix on the computer
+
+```zsh
+sh <(curl -L https://nixos.org/nix/install) --daemon
+```
+
+Install home-manager
+
+```zsh
+nix-channel --add https://github.com/nix-community/home-manager/archive/master.tar.gz home-manager
+nix-channel --update
+nix-shell '<home-manager>' -A install
+```
+
+Install packages and create configuration files with home-manager
+
+```zsh
+home-manager switch
+```
+
+### Git
+
+Configure $GITUSER, $GITEMAIL
+
+```zsh
+export $GITUSER="<git name>"
+export $GITEMAIL="<git email>"
+```
+
+### Tmux
+
+Retrieve tpm plugin submodule in tmux/plugins/tmux
+
+```zsh
+git submodules update --init
+```
--- installer.sh
+++ installer.sh
@@ -29,10 +29,6 @@ echo removed home-manager from $XDG_CONFIG_HOME/.config
 rm -rf ~/.tmux
 echo removed .tmux directory from $HOME
 
-# tmux package manager installation
-# git clone https://github.com/tmux-plugins/tpm ~/dotfiles/tmux/plugins/tpm
-# echo cloned tmux package manager to dotfiles tmux directory
-
 # create ~/.config if it doesn't exist already
 mkdir -p ~/.config
 
@@ -50,16 +46,10 @@ echo linked zsh configurations to $XDG_CONFIG_HOME
 ln -s $HOME/dotfiles/home-manager $HOME/.config
 echo linked home-manager configuration to $XDG_CONFIG_HOME
 
-# sh <(curl -L https://nixos.org/nix/install) --daemon
-#
-# nix-channel --add https://github.com/nix-community/home-manager/archive/master.tar.gz home-manager
-# nix-channel --update
-#
-# nix-shell '<home-manager>' -A install
-
 # copy fonts to local machine for mac
 if [[ $OSTYPE == 'darwin'* ]]; then
   cp -rf $HOME/dotfiles/fonts/*.ttf ~/Library/Fonts
   echo copied fonts on mac system
 fi
 
+git submodule update --init
