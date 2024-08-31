72f0e1f60a43d333d1b894151a62e72bfacdf02f
Chad W Pry <chad.pry@gmail.com>
2024-08-30 20:15:32 -0500

    remove bash references to NVM

--- home-manager/home.nix
+++ home-manager/home.nix
@@ -57,11 +57,6 @@
     programs.bash = {
       enable = true;
       enableCompletion = true;
-      # profileExtra = ''
-      #   export NVM_DIR="$HOME/.nvm"
-      #   [ -s "$NVM_DIR/nvm.sh" ] && \. "$NVM_DIR/nvm.sh"  # This loads nvm
-      #   [ -s "$NVM_DIR/bash_completion" ] && \. "$NVM_DIR/bash_completion"  # This loads nvm bash_completion
-      # '';
     };
 
     programs.bat = {
